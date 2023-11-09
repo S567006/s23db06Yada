da0a255a40fabdc39842833ddcba7ace956a7731
S567006 <143199322+S567006@users.noreply.github.com>
2023-11-08 21:17:24 -0600

    updated app.js

--- routes/peacock.js
+++ routes/peacock.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const peacock = mongoose.Schema({
-costume_type: String,
-size: String,
-cost: Number
+peacock_color: String,
+peacock_breed: String,
+peacock_price: Number
 })
 module.exports = mongoose.model("Costume",
 peacock)
