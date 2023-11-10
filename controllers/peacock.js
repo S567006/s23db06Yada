bc6aa0f9eda2a1c4d66cec5799596b5e0be2404f
S567006 <143199322+S567006@users.noreply.github.com>
2023-11-10 11:54:51 -0600

    updating peacock.js and resource.js

--- controllers/peacock.js
+++ controllers/peacock.js
@@ -58,3 +58,15 @@ exports.peacock_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+// for a specific peacock.
+exports.peacock_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await peacock.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
--- routes/resource.js
+++ routes/resource.js
@@ -27,3 +27,23 @@ res.write('}');
 res.write(']')
 res.send();
 };
+// Handle peacock update form on PUT.
+exports.peacock_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await peacock.findById( req.params.id)
+// Do updates of properties
+if(req.body.peacock_color)
+toUpdate.peacock_color = req.body.peacock_color;
+if(req.body.peacock_breed) toUpdate.peacock_breed = req.body.peacock_breed;
+if(req.body.peacock_price) toUpdate.peacock_price = req.body.peacock_price;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
